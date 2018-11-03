fe915b474ce92600cc139cd245c93f50f6386605
Mateus <mateushcos@gmail.com>
2018-11-02 23:27:08 -0300

    Interface/Perfil

---
+++ app/src/main/java/com/example/jeffe/trabalho_final/ProfileActivity.java
@@ -0,0 +1,14 @@
+package com.example.jeffe.trabalho_final;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
